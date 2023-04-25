e2f83e6f4e35ffdb26d4cf2de97456bdbd1ac564
Aman Sharma <57478679+amansharma312@users.noreply.github.com>
2023-04-25 13:45:14 +0530

    Create qr.java

---
+++ QR/src/main/java/qr.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class qr {
+    public static void main(String[] args){
+        
+        //scanning the qr from the machine 
+        Scanner sc = new Scanner(System.in);
+        System.out.print("input: ");
+        String input = sc.next();
+        System.out.println("read input: "+ input);
+    }
+}
